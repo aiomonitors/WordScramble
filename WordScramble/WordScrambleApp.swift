de4ce888e8eeeda0ac44dfe1c30fe258549511ca
aiomonitors <sachow168@gmail.com>
2023-04-11 15:56:50 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Shihab Chowdhury on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
